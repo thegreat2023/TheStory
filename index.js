99326c7d0efa47f5d79831ea8d1de339f271ccbe
thegreat2023 <143896392+thegreat2023@users.noreply.github.com>
2024-08-04 12:14:17 +0530

    index.js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const http = require("http");
-const express = require("express");
-const path = require("path");
-const { Server } = require("socket.io");
+const http = require('http');
+const express = require('express');
+const path = require('path');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Handle Socket.io connections
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("user-message", (message) => {
-    io.emit("message", message); // Broadcast the message to all connected clients
-  });
-});
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files (none in this example, but useful for future extensions)
+app.use(express.static(path.join(__dirname)));
 
 // Serve the index.html file
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Handle Socket.io connections
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  
+  socket.on('user-message', (message) => {
+    io.emit('message', message); // Broadcast the message to all connected clients
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 // Start the server
